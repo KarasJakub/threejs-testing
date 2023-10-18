dfd27f0cf24ae464e9e63935868c96ccc9ea1f21
Jakub Karaś <jakub.karas@onet.pl>
2023-10-18 23:37:51 +0200

    testing animations

---
+++ src/test.js
@@ -0,0 +1,36 @@
+import * as THREE from "three"
+
+// const scene = new THREE.Scene()
+// const camera = new THREE.PerspectiveCamera(
+//   24,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// )
+
+// const renderer = new THREE.WebGLRenderer()
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// document.body.appendChild(renderer.domElement)
+
+// const colorYellow = new THREE.Color("hsl(40, 100%, 50%)")
+// const colorPink = new THREE.Color("hsl(360, 100%, 50%)")
+
+// const cubeGeometry = new THREE.BoxGeometry(1, 1.5, 0.9)
+// const cubeMaterial = new THREE.MeshPhongMaterial({
+//   color: colorYellow,
+// })
+
+// const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+// const light = new THREE.PointLight(colorPink, 2)
+
+// light.position.z = 20
+// light.position.y = -20
+// light.position.x = -40
+
+// scene.add(light)
+// scene.add(cube)
+
+// camera.position.z = 15
+
+// renderer.render(scene, camera)
